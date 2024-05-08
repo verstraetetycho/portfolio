4795dabd305c741344b994af77775af5bcea237e
Tycho Verstraete <tycho@tychoverstraete.be>
2024-05-08 13:52:50 +0200

    Fix Tailwind in production

--- src/client.tsx
+++ src/client.tsx
@@ -1,1 +1,1 @@
-import './index.css';
+import '../public/static/css/index.css';
--- src/index.tsx
+++ src/index.tsx
@@ -2,48 +2,46 @@ import { Hono } from 'hono';
 import { reactRenderer } from '@hono/react-renderer';
 import { Home } from './modules/home';
 
-const isProd = process.env.IS_PROD === 'true';
-
-const manifestPath = '../dist/manifest.json';
-const cssFile = isProd
-  ? (await import(manifestPath)).default['src/client.tsx']?.css?.at(0)
-  : null;
-
 const app = new Hono();
 
 app.get(
   '*',
-  reactRenderer(({ children, title }) => {
-    return (
-      <html lang='en'>
-        <head>
-          <meta charSet='UTF-8' />
-          <meta
-            name='viewport'
-            content='width=device-width, initial-scale=1.0'
-          />
-          <link href='/static/css/reset.css' rel='stylesheet' />
-          {cssFile && <link rel='stylesheet' href='${cssFile}' />}
-          <title>{`${title} | Tycho Verstraete`}</title>
-        </head>
-        <body>
-          {children}
-          {!isProd && (
-            <>
-              <script
-                type='module'
-                src='http://localhost:5173/@vite/client'
-              ></script>
-              <script
-                type='module'
-                src='http://localhost:5173/src/client.tsx'
-              ></script>
-            </>
-          )}
-        </body>
-      </html>
-    );
-  })
+  reactRenderer(
+    ({ children, title, c }) => {
+      return (
+        <html lang='en'>
+          <head>
+            <meta charSet='UTF-8' />
+            <meta
+              name='viewport'
+              content='width=device-width, initial-scale=1.0'
+            />
+            <link href='/static/css/reset.css' rel='stylesheet' />
+            <link href='/static/css/index.css' rel='stylesheet' />
+            <title>{`${title} | Tycho Verstraete`}</title>
+          </head>
+          <body>
+            {children}
+            {c.env.IS_PROD !== 'true' && (
+              <>
+                <script
+                  type='module'
+                  src='http://localhost:5173/@vite/client'
+                ></script>
+                <script
+                  type='module'
+                  src='http://localhost:5173/src/client.tsx'
+                ></script>
+              </>
+            )}
+          </body>
+        </html>
+      );
+    },
+    {
+      docType: true,
+    }
+  )
 );
 
 app.get('/', (c) => {
